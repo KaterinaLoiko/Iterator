1529638e57c7338996b5d6e6f98d9ce2a1d36862
ekaterinalojko <eloiko@energocomm.ru>
2023-03-18 18:38:30 +0300

    init commit

---
+++ src/com/homework/RandomIterator.java
@@ -0,0 +1,16 @@
+package com.homework;
+
+import java.util.Iterator;
+
+public class RandomIterator implements Iterator<Integer> {
+
+  @Override
+  public boolean hasNext() {
+    return false;
+  }
+
+  @Override
+  public Integer next() {
+    return null;
+  }
+}
